dea299e585c8146f4d4aaf1fc3f4cbfa4d062e64
LaurenSaggar <saggar.4@osu.edu>
2020-11-22 00:49:40 -0500

    Finish user static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'user_static_pages/loginPage'
+  root 'user_static_pages#loginPage'
   get 'user_static_pages/projectPage'
   get 'user_static_pages/groupPage'
   get 'user_static_pages/ratingPage'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
--- test/controllers/user_static_pages_controller_test.rb
+++ test/controllers/user_static_pages_controller_test.rb
@@ -2,23 +2,27 @@ require 'test_helper'
 
 class UserStaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get loginPage" do
-    get user_static_pages_loginPage_url
+    get root_url
     assert_response :success
+    assert_select "title", "Login | Peer Evaluation Tool"
   end
 
   test "should get projectPage" do
     get user_static_pages_projectPage_url
     assert_response :success
+    assert_select "title", "Projects | Peer Evaluation Tool"
   end
 
   test "should get groupPage" do
     get user_static_pages_groupPage_url
     assert_response :success
+    assert_select "title", "Group | Peer Evaluation Tool"
   end
 
   test "should get ratingPage" do
     get user_static_pages_ratingPage_url
     assert_response :success
+    assert_select "title", "Ratings | Peer Evaluation Tool"
   end
 
 end
