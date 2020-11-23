fd94310cfc43af2531734433de6155bd06b8dd3c
anaik07 <naik.89@osu.edu>
2020-11-23 12:04:16 -0500

    added skeleton for admin pages

---
+++ app/controllers/admin_static_pages_controller copy.rb
@@ -0,0 +1,20 @@
+class AdminStaticPagesController < ApplicationController
+  def loginPage
+  end
+
+  def projectPage
+  end
+
+  def groupPage
+  end
+
+  def groupMemberPage
+  end
+
+  def studentPage
+  end
+
+  def ratingPage
+  end
+  
+end
---
+++ app/helpers/admin_static_pages_helper copy.rb
@@ -0,0 +1,2 @@
+module AdminStaticPagesHelper
+end
