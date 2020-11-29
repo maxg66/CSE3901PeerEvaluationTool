ed6353eb7e28a74d1f300b851c6b8f3b57eeb7b5
Tejas Venkatachalam <tejasv526@gmail.com>
2020-11-29 18:17:16 -0500

    You can now add Projects, but still need to add verifications

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'user_static_pages/groupPage'
   get 'user_static_pages/ratingPage'
   post '/loginVal' => 'user_static_pages#loginPageVal'
+  post '/addProject' => 'admin_static_pages#valAddProject'
+  get '/navAddProj' => 'admin_static_pages#createProjectPage'
   #get 'admin_static_pages/loginPage'
   get 'admin_static_pages/projectPage'
   get 'admin_static_pages/groupPage'
