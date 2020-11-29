4a8cf0d562a5c6d9a0b8f1f0b8e6461bdad11267
LaurenSaggar <saggar.4@osu.edu>
2020-11-29 18:38:04 -0500

    create new controllers for models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'evaluations/new'
+  get 'projects/new'
+  get 'groups/new'
+  get 'users/new'
+  get '/addMember',  to:'admins#new'
   resources :groups
   root 'user_static_pages#loginPage'
   get 'user_static_pages/projectPage'
@@ -11,4 +16,9 @@ Rails.application.routes.draw do
   get 'admin_static_pages/groupMemberPage'
   get 'admin_static_pages/ratingPage'
   #get 'admin_static_pages/studentPage'
+
+  # get '/projectPage',     to: 'user_static_pages#projectPage'
+  # get '/groupPage',       to: 'user_static_pages#groupPage'
+  # get '/ratingPage',      to: 'user_static_pages#ratingPage'
+  # post '/loginVal',       to: 'user_static_pages#loginPageVal'
 end
