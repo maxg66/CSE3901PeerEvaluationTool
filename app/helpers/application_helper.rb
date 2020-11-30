1377d8213d1f30df49e8b7f352d44aea98315372
Maxwell Giffin <giffin.224@osu.edu>
2020-11-30 17:05:36 -0500

    bootstrap components and styling for admin project and group pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+    def link_to_button(text, path)
+        "<button class=\"btn btn-primary btn-lg btn-block\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
+    end
 end
