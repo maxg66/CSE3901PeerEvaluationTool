94ae03ab0c1003f592e98b8c6e41a65edf837fd1
Maxwell Giffin <giffin.224@osu.edu>
2020-11-30 18:35:42 -0500

    bootstrap for project and group pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def link_to_button(text, path)
         "<button class=\"btn btn-primary btn-lg btn-block\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
     end
+
+    def link_to_primary_button(text, path)
+        "<button class=\"btn btn-primary\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
+    end
 end
