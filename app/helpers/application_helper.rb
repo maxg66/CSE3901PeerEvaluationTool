bfd7c993c3be6e10d608a547f80232e35cea5349
Maxwell Giffin <giffin.224@osu.edu>
2020-12-02 21:34:56 -0500

    update buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
     def link_to_button(text, path)
-        "<button class=\"btn btn-primary btn-lg btn-block\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
+        "<button class=\"btn btn-danger btn-lg btn-block\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
     end
 
     def link_to_primary_button(text, path)
-        "<button class=\"btn btn-primary\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
+        "<button class=\"btn btn-danger\" onclick=\"document.location='#{path}'\">#{text}</button>".html_safe
     end
 end
